39be513ffe6c36c86ba8949da53a0597f3682923
nicole0807-glitch <nicole.tolve@correo.unimet.edu.ve>
2025-02-13 18:00:03 -0400

    secuencia arregla y diseño responsive

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 "use strict";
-const round = document.getElementById('round');
+/*const round = document.getElementById('round');*/
+const round1= document.getElementById('valorronda');
 const botonesJuego = document.getElementsByClassName('Boton');
 const botonInicio = document.getElementById('botonjugar');
 
 
 class SimonSays{
-    constructor(botonesJuego, botonInicio, round){
-        this.round = 0;
+    constructor(botonesJuego, botonInicio, round1){
+       /* this.round = 0;*/
+        this.round1 = 0;
+        this.score = 0;
         this.totalRounds = 10;
         this.posicionUsuario = 0;
         this.secuencia = [];
@@ -15,8 +18,9 @@ class SimonSays{
         this.velocidad = 1000;
         this.display = {
             botonInicio,
-            round
+            round1
         }
+        this.valorRondaElement = round1;
         this.sonidoError = new Audio('./sounds/error.wav');
         this.sonidoBoton = [
             new Audio('./sounds/1.mp3'),
@@ -29,12 +33,13 @@ class SimonSays{
 
     init(){
         this.display.botonInicio.onclick = () => this.comenzarJuego();
+       /* this.mostrarPuntaje();*/
     }
     
     //Comienza el juego
     comenzarJuego(){
         this.display.botonInicio.disabled = true;
-        this.actualizarRound(0);
+        this.actualizarRound(1);
         this.posicionUsuario = 0;
         this.secuencia = this.generarSecuencia();
         this.botones.forEach((element, i) => {
@@ -44,10 +49,13 @@ class SimonSays{
         this.mostrarSecuencia();
     }
 
-    //Actualiza el valor de Round a medida que se avanza y se muestra en pantalla
-    actualizarRound(valor){
-        this.round = valor;
-        this.display.round.textContent = `Round ${this.round}`;
+      // Actualiza el valor de Round a medida que se avanza y se muestra en pantalla
+      actualizarRound(valor) {
+        /* this.round = valor;*/
+        this.round1 = valor;
+        this.valorRondaElement.textContent = this.round1;
+        this.valorRondaElement.setAttribute('data-round', this.round1);
+        /* this.display.round.textContent = `Round ${this.round}`;*/
     }
 
 
@@ -66,10 +74,13 @@ class SimonSays{
     validarColor(valor){
         if(this.secuencia[this.posicionUsuario]=== valor){
            this.sonidoBoton[valor].play(); // para que suene el boton cuando este bien la respuesta
-            if(this.round === this.posicionUsuario){
-                this.actualizarRound(this.round + 1);
-                this.speed /= 1.02;
-                this.isjuegoPerdido();
+            if(this.posicionUsuario === this.round1 - 1){
+                /*this.actualizarScore();*/
+                //this.mostrarPuntaje(); //esta comentado porque aun no esta bien programado 
+                // e impide el buen funcionamiento del juego !!arreglar!!
+                this.actualizarRound(this.round1 + 1);
+                this.velocidad/= 1.02;
+                this.isJuegoPerdido();
             } else {
                 this.posicionUsuario++;
             }
@@ -78,8 +89,14 @@ class SimonSays{
         }
     }
 
-    isjuegoPerdido(){
-        if(this.round === this.totalRounds){
+    isJuegoPerdido(){
+     /*   if(this.round === this.totalRounds){
+            this.juegoGanado();
+        } else {
+            this.posicionUsuario = 0;
+            this.mostrarSecuencia();
+    }*/
+        if(this.round1 === this.totalRounds){
             this.juegoGanado();
         } else {
             this.posicionUsuario = 0;
@@ -93,17 +110,24 @@ class SimonSays{
         let indexSecuencia = 0;
         let temporizador = setInterval(() => {
             const boton = this.botones[this.secuencia[indexSecuencia]];
-            this.sonidoBoton[this.secuencia[indexSecuencia]].play();//para que suene el boton
+            this.sonidoBoton[this.secuencia[indexSecuencia]].play(); // para que suene el boton
             this.altenarBotones(boton);
             setTimeout(() => this.altenarBotones(boton), this.velocidad / 2);
             indexSecuencia++;
-            if(indexSecuencia > this.round){
+
+            if (indexSecuencia >= this.round1) {
                 this.botonesBloqueados = false;
                 clearInterval(temporizador);
             }
         }, this.velocidad);
     }
+           /* if(indexSecuencia > this.round){
+                this.botonesBloqueados = false;
+                clearInterval(temporizador);
+            }
+        }, this.velocidad);*/
 
+       
     //revisar
     altenarBotones(boton){
         boton.classList.toggle('active'); //para que se vea el cambio de color osea que hay que acomodar en el css
@@ -123,7 +147,18 @@ class SimonSays{
         this.botones.forEach(element => { element.classList.add("ganador")});
     }
 
-}
+    /*actualizarScore(){
+        let puntajeActual = localStorage.getItem('puntaje') || 0;
+        puntajeActual = parseInt(puntajeActual) + 1;
+        localStorage.setItem('puntaje', puntajeActual);
+    }*/
 
-const simon = new SimonSays(botonesJuego, botonInicio, round);
+    //agregar al html el puntaje
+    /*mostrarPuntaje(){
+        const puntaje = localStorage.getItem('puntaje') || 0;
+        this.display.puntaje.textContent = `Puntaje: ${puntaje}`;
+    }*/
+
+}
+const simon = new SimonSays(botonesJuego, botonInicio, round1);
 simon.init();
